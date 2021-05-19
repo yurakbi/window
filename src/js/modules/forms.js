bfd1303c55c3af440393ecb55f424204d3a949ec
yura <yura04091@gmail.com>
2021-05-19 14:26:12 +0300

    first commit

---
+++ src/js/modules/forms.js
@@ -0,0 +1,12 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        input = document.querySelectorAll('input');
+    
+    const messages = {
+        loading: 'Загрузка...',
+        success: 'Дякуємо! Скоро з вами звяжуться',
+        failure: 'Щось пішло не так...'
+    }
+};
+
+export default forms;
