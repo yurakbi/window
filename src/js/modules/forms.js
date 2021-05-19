5f40081ac0f0cbecb85a69cbf5e5d3801d02f7f0
yura <yura04091@gmail.com>
2021-05-19 17:33:27 +0300

    forms

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,12 +1,61 @@
 const forms = () => {
     const form = document.querySelectorAll('form'),
-        input = document.querySelectorAll('input');
+        inputs = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
     
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, "");
+        });
+    });
+
     const messages = {
         loading: 'Загрузка...',
         success: 'Дякуємо! Скоро з вами звяжуться',
         failure: 'Щось пішло не так...'
-    }
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = messages.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = messages.success;
+                })
+                .catch(() => statusMessage.textContent = messages.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+
+        });
+    });
 };
 
 export default forms;
